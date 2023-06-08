2b251062940ac08a3a4f9c4e032f1b8c4726c9cb
ImranRovano <imranhuhn1@gmail.com>
2023-06-08 15:16:37 -0400

    /:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    variants: {
+      extend: {
+        ringWidth: ["focus-visible"],
+        ringColor: ["focus-visible"],
+        ringOpacity: ["focus-visible"],
+        ringOffsetWidth: ["focus-visible"],
+        ringOffsetColor: ["focus-visible"],
+      },
+      backgroundColor: ["active"],
     },
   },
   plugins: [],
-}
+};
